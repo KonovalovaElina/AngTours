ea3aab7a6e249e9beda0cc86c817efd295833825
Элина Коновалова <konovalova.elina@gmail.com>
2026-09-09 22:36:42 +0300

    Add tabs

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-auth',
-  imports: [AuthorizationComponent, RegistrationComponent],
+  imports: [AuthorizationComponent, RegistrationComponent, MatTabsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })

2d0329b6c27a4a8dd87393319709ac61ef6fbf2d
Элина Коновалова <konovalova.elina@gmail.com>
2026-09-10 00:25:48 +0300

    Update registration

--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -1,9 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [NgClass, FormsModule, MatButtonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent {}
+export class RegistrationComponent {
+  login = '';
+  password = '';
+  passwordRepeat = '';
+  email = '';
+
+  onReg(ev: Event): void {
+    if (this.login) {
+      localStorage.setItem('user', this.login);
+    }
+  }
+}

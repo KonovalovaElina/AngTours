888ce4b230e484357fd9b0df552ee760c08f281c
Элина Коновалова <konovalova.elina@gmail.com>
2026-09-10 01:14:25 +0300

    Update authorization

--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,9 +1,30 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-authorization',
-  imports: [],
+  imports: [NgClass, FormsModule, MatButtonModule],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss',
 })
-export class AuthorizationComponent {}
+
+export class AuthorizationComponent {
+  login = '';
+  password = '';
+
+  onAuth(ev: Event): void {
+    const savedUser = localStorage.getItem('user');
+
+    if (savedUser) {
+      if (this.login === savedUser) {
+        console.log('Логин совпадает, авторизация успешна');
+      } else {
+        alert('Неверный логин');
+      }
+    } else {
+      localStorage.setItem('user', this.login);
+      alert('Логин сохранён как основной');
+    }
+  }
+}

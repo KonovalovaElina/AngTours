ce5f8326cb28fb2b48724d1b69dfb73f0f7d9d48
Элина Коновалова <konovalova.elina@gmail.com>
2026-09-18 19:12:25 +0300

    Add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './pages/auth/auth.component';
 import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
@@ -10,7 +9,7 @@ export const routes: Routes = [
   },
   {
     path: 'auth', 
-    component: AuthComponent
+    loadComponent: () => import('./pages/auth/auth.component').then(c => c.AuthComponent)
   }
   
 ];

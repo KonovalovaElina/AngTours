5faa9ee4b305370427059c49a6768cb35f241fb8
Элина Коновалова <konovalova.elina@gmail.com>
2026-09-18 19:12:57 +0300

    Update authorization

--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,44 +1,59 @@
 import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { UserService } from '../../../services/user.service';
+import { UserApiService } from '../../../services/api/user-api.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-authorization',
-  imports: [NgClass, FormsModule, MatButtonModule, MatCheckboxModule],
+  imports: [NgClass, FormsModule, MatButtonModule, MatCheckboxModule, MatSnackBarModule],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss',
 })
 
-export class AuthorizationComponent {
+export class AuthorizationComponent implements OnInit, OnDestroy {
   private userService = inject(UserService);
+  private router = inject(Router);
   login = '';
   password = '';
   saveInStore = false;
+  userApiService = inject(UserApiService);
 
-//   onAuth(ev: Event): void {
-//     const savedUser = localStorage.getItem('user');
+  private _snackBar = inject(MatSnackBar);
 
-//     if (savedUser) {
-//       if (this.login === savedUser) {
-//         console.log('Логин совпадает, авторизация успешна');
-//       } else {
-//         alert('Неверный логин');
-//       }
-//     } else {
-//       localStorage.setItem('user', this.login);
-//       alert('Логин сохранён как основной');
-//     }
-//   }
-// }
+  constructor(private userService2: UserService) {
+    console.log('constr init');
+  }
+
+  ngOnInit(): void {
+    console.log('auth init');
+  }
+
+  ngOnDestroy(): void {
+    console.log('auth destroy');
+  }
 
   onAuth(ev: Event): void {
-    if (this.saveInStore) {
-      this.userService.saveUserInStore({login: this.login});
-    } else {
-      this.userService.setUser({login: this.login});
-    }
+    this.userApiService.auth({login: this.login, password: this.password}).subscribe (() => {
+      if (this.saveInStore) {
+        this.userService.saveUserInStore({login: this.login});
+      } else {
+        this.userService.setUser({login: this.login});
+      }
+      this.router.navigate(["/"]);
+    },
+    (err) => {
+      this._snackBar.open('Ошибка авторизации. Проверьте логин и пароль.', 'Закрыть', {
+        duration: null, // Не закрываем автоматически — пусть пользователь сам закроет
+        panelClass: ['error-snackbar'],
+        verticalPosition: 'top',
+        horizontalPosition: 'center'
+      });
+    });
+
   }
 
 }
---
+++ src/app/services/api/user-api.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from "@angular/core";
+import { API } from "../../shared/api";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IAuthorization } from "../../models/authorization";
+
+@Injectable ({
+  providedIn: 'root'
+})
+
+export class UserApiService {
+  private api = API;
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  auth(body: IAuthorization): Observable<IAuthorization> {
+    return this.http.post<IAuthorization>(this.api.auth, body)
+  }
+}

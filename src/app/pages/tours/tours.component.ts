2e36ac26a0bd108c8602a0d87acf66b544dd3b57
Элина Коновалова <konovalova.elina@gmail.com>
2026-09-24 23:45:55 +0300

    Update tours.component

--- src/app/pages/tours/tours.component.ts
+++ src/app/pages/tours/tours.component.ts
@@ -9,10 +9,11 @@ import { ToursService } from '../../services/tours.service';
 })
 export class ToursComponent implements OnInit {
   private toursService = inject(ToursService);
+  tours: any;
 
   ngOnInit(): void {
-    this.toursService.getTours().subscribe((data) => {
-      console.log('data tours', data)
+    this.toursService.getTours().subscribe((data: any) => {
+      this.tours = data.tours;
     })
   }
 }
